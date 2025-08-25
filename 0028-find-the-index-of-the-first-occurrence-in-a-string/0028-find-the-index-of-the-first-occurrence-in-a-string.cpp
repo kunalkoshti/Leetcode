0792db7cdab9f98f124c188e8b3ba87c16c638f8
Kunal Koshti <69470650+kunalkoshti@users.noreply.github.com>
2025-08-25 06:46:18 +0530

    Time: 2 ms (9.33%), Space: 9.2 MB (5.28%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -14,14 +14,35 @@ public:
         return res;
     }
 
+    vector<int> z_funct(string s) {
+        int n = s.length();
+        int l = 0, r = 0;
+        vector<int> z(n, 0);
+        for (int i = 1; i < n; i++) {
+            if (i < r)
+                z[i] = min(r - i, z[i - l]);
+            for (int j = z[i]; j < n - i; j++) {
+                if (s[j] == s[i + j])
+                    z[i]++;
+                else
+                    break;
+            }
+            if (i + z[i] > r) {
+                l = i, r = i + z[i];
+            }
+        }
+        return z;
+    }
+
     int strStr(string haystack, string needle) {
         string t = needle + "#" + haystack;
         int n = needle.length();
         int m = haystack.length();
-        vector<int> pi = prefix_funct_opt(t);
-        for (int i = 2 * n; i < t.length(); i++) {
-            if (pi[i] == n)
-                return i - 2 * n;
+        //vector<int> pi = prefix_funct_opt(t);
+        vector<int> z = z_funct(t);
+        for (int i = n+1; i < t.length(); i++) {
+            if (z[i] == n)
+                return i - (n+1);
         }
         return -1;
     }
